aaeb3aa4816256c62ff0b951b8d078c70b380953
FirdausMat <117062512+FirdausMat@users.noreply.github.com>
2022-11-15 16:14:46 +0800

    update

--- abc/src/main/java/abc/greets.java
+++ abc/src/main/java/abc/greets.java
@@ -4,7 +4,7 @@ public class greets {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello XD");
+		System.out.println("Hello XXD");
 
 	}
 
